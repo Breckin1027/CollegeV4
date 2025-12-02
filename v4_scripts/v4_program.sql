83cc99322855df35899b6f285679c3a28ccde70f
Breckin1027 <BrLuke01@wsc.edu>
2025-12-02 13:44:39 -0600

    Created the base for the Insert Trigger

--- v4_scripts/v4_program.sql
+++ v4_scripts/v4_program.sql
@@ -1,4 +1,32 @@
 /*
 Create a system to set user_id and email to WSC format
 Add triggers, audit fields, and maintain data added/modified
+
+Step 1: Set up audit timestamp columns to auto update for both DEFAULT and UPDATE (If not complete already)
+Step 2: Add a system generated userid and campus email to the user table WSC style
+		For this, use an insert trigger to maintain these two new columns
+Step 3: Convert audit userid fields from int to string so the first part of CURRENT_USER can be stored
+		Use an insert and update triggers to maintain these columns
+        Include logic to standardize data, like gender being upper case F/M/X
+Step 4: Create an audit table, triggers for insert, update, and delete, and events to truncate every month        
 */
+
+DELIMITER //
+
+CREATE TRIGGER campus_id_before_insert
+	BEFORE INSERT ON user
+	FOR EACH ROW
+BEGIN 
+	SET prefix_id = CONCAT(LOWER(SUBSTRING(NEW.first_name, 1, 2)), LOWER(SUBSTRING(NEW.last_name, 1, 4)));
+	
+    SELECT MAX(CAST(SUBSTRING(campus_id, LENGTH(prefix_id)+1, 1) AS UNSIGNED))
+    INTO max_suffix
+    FROM user
+    WHERE campus_id LIKE (prefix_id, '%');
+    /*
+    IF max_suffix IS NULL THEN
+		continue
+	END IF;
+	*/	
+END//
+    
