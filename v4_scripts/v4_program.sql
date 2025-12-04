cc8aa1c018b72f4a8c172a76cd5564c4a5783a02
Breckin1027 <BrLuke01@wsc.edu>
2025-12-03 23:25:32 -0600

    Modified audit fields and implemented INSERT and UPDATE triggers to maintain these changes.

--- v4_scripts/v4_program.sql
+++ v4_scripts/v4_program.sql
@@ -2,16 +2,19 @@
 Create a system to set user_id and email to WSC format
 Add triggers, audit fields, and maintain data added/modified
 
-Step 1: Set up audit timestamp columns to auto update for both DEFAULT and UPDATE (If not complete already)
-Step 2: Add a system generated userid and campus email to the user table WSC style
+Complete: 
+	Step 1: Set up audit timestamp columns to auto update for both DEFAULT and UPDATE (If not complete already)
+	Step 2: Add a system generated userid and campus email to the user table WSC style
 		For this, use an insert trigger to maintain these two new columns
-Step 3: Convert audit userid fields from int to string so the first part of CURRENT_USER can be stored
+	Step 3: Convert audit userid fields from int to string so the first part of CURRENT_USER can be stored
 		Use an insert and update triggers to maintain these columns
+To Do:
         Include logic to standardize data, like gender being upper case F/M/X
-Step 4: Create an audit table, triggers for insert, update, and delete, and events to truncate every month        
+	Step 4: Create an audit table, triggers for insert, update, and delete, and events to truncate every month        
 */
 
 DROP TRIGGER IF EXISTS campus_id_before_insert;
+DROP TRIGGER IF EXISTS campus_id_before_update;
 
 DELIMITER //
 
@@ -23,6 +26,12 @@ BEGIN
     DECLARE max_suffix  VARCHAR(255);
     
 	SET prefix_id = CONCAT(LOWER(SUBSTRING(NEW.first_name, 1, 2)), LOWER(SUBSTRING(NEW.last_name, 1, 4)));
+    
+    SET NEW.created = NOW();
+    SET NEW.updated = NOW();
+    
+    SET NEW.created_userid = CURRENT_USER();
+    SET NEW.updated_userid = CURRENT_USER();
 	
     SELECT MAX(CAST(SUBSTRING(campus_id, LENGTH(prefix_id)+1, 1) AS UNSIGNED))
     INTO max_suffix
@@ -30,4 +39,15 @@ BEGIN
     WHERE campus_id LIKE CONCAT(prefix_id, '%');
     
 END//
+
+
+DELIMITER //
+
+CREATE TRIGGER campus_id_before_update
+	BEFORE UPDATE ON user
+    FOR EACH ROW
+BEGIN 
+    SET NEW.updated = NOW();
+    SET NEW.updated_userid = CURRENT_USER();
     
+END //
